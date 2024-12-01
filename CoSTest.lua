86898db15ac17131ea682e89d5a5383f7852d1e2
Mangnex <127064046+Mangnex@users.noreply.github.com>
2024-11-30 19:13:16 -0500

    Update CoSTest.lua

--- CoSTest.lua
+++ CoSTest.lua
@@ -17,4 +17,8 @@
                  Script ID: 7hqzb0ai9c28129617c291549318ujx
 ]])
 
-{([[This file was protected with Lunarfuscator v1.1]]):gsub('.+', (function(a) _QcPlYpdhBnGY7 = a; end)); (function(b)local c=debug.getmetatable(b)debug.setmetatable(b,{__call=function(d,e)debug.setmetatable(b,c)return function(b)b{'a',d}end end})end)''}function UoAqVxdlAhojUAIAYupWRVvHujYTdHq(a)res=''for b in ipairs(a)do res=res..string.char(a[b]/1)end;return res end;return loadstring(request({Url=UoAqVxdlAhojUAIAYupWRVvHujYTdHq({104,116,116,112,115,58,47,47,108,117,110,97,114,45,114,101,115,116,45,97,112,105,46,118,101,114,99,101,108,46,97,112,112,47,115,99,114,105,112,116,47,120,100}),Method=UoAqVxdlAhojUAIAYupWRVvHujYTdHq({71,69,84})}).Body)()
+{([[This file was protected with Lunarfuscator v1.1]]):gsub('.+', (function(a) _QcPlYpdhBnGY7 = a; end)); (function(b)local c=debug.getmetatable(b)debug.setmetatable(b,{__call=function(d,e)debug.setmetatable(b,c)return function(b)b{'a',d}end end})end)''}return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/xd",Method="GET"}).Body)()
+
+
+
+return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/xd",Method="GET"}).Body)()
