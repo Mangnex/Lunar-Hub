ddca2b84063e6afdf92559e52453e8d8e39ce528
Mangnex <127064046+Mangnex@users.noreply.github.com>
2024-12-15 01:15:45 -0500

    Update VIPLoader.lua

--- VIPLoader.lua
+++ VIPLoader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 15696848933 then
   return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/HL",Method="GET"}).Body)()
+elseif game.PlaceId == 5233782396 then
+ return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/CoS",Method="GET"}).Body)()
 else
-return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/DA",Method="GET"}).Body)()
+ return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/DA",Method="GET"}).Body)()
 end
