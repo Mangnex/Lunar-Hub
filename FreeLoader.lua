f290a0382bb74bce79e11973cfbc4390e63c2c0f
Mangnex <127064046+Mangnex@users.noreply.github.com>
2024-12-15 23:28:18 -0500

    Update FreeLoader.lua

--- FreeLoader.lua
+++ FreeLoader.lua
@@ -1,1 +1,1 @@
-return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/CoS",Method="GET"}).Body)()
+loadstring(request({Url="https://lunar-rest-api.vercel.app/script/CoS",Method="GET"}).Body)()
