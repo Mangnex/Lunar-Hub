b4875628f97b625fe4b7a3ad498eda1b61e6ebad
Mangnex <127064046+Mangnex@users.noreply.github.com>
2025-01-04 20:22:59 -0500

    Update FreeLoader.lua

--- FreeLoader.lua
+++ FreeLoader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 3431407618 then
-    return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/isle10",Method="GET"}).Body)()
+    return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/Isle10",Method="GET"}).Body)()
 else
    return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/CoS",Method="GET"}).Body)()
 end
