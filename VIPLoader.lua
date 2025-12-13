08a17ad4dbb1ee560156db0a40176d9ed07f1438
Mangnex <127064046+Mangnex@users.noreply.github.com>
2025-12-13 02:58:59 -0500

    Update script URL for specific PlaceId

--- VIPLoader.lua
+++ VIPLoader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 15696848933 or game.PlaceId == 123063406055708 then
   return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/HL",Method="GET"}).Body)()
 elseif game.PlaceId == 5233782396 then
- return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/CoS",Method="GET"}).Body)()
+ return loadstring(request({Url='https://api.catsec.xyz/script/6q1wq2no9mev7th6u12io5ov12269z5w',Method='GET'}).Body)()
 else
  return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/DA",Method="GET"}).Body)()
 end
