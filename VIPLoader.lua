e8b9c8ebe3e867dda95e17f437d47270f539d24a
Mangnex <127064046+Mangnex@users.noreply.github.com>
2026-01-27 11:58:20 -0500

    Update script loading logic for specific PlaceIds

--- VIPLoader.lua
+++ VIPLoader.lua
@@ -1,7 +1,8 @@
 if game.PlaceId == 15696848933 or game.PlaceId == 123063406055708 then
   return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/HL",Method="GET"}).Body)()
 elseif game.PlaceId == 5233782396 then
- return loadstring(request({Url='https://api.catsec.xyz/script/6q1wq2no9mev7th6u12io5ov12269z5w',Method='GET'}).Body)()
+  getgenv().SCRIPT_KEY = ""
+ return loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/ecb44bf5d377aae4932921c46820a9d76446200a0a2aef2477c1cbf24a99473e/download"))()
 else
  return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/DA",Method="GET"}).Body)()
 end
