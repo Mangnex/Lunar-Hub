438c2cf2566246f940509eacca5ad6ec311e4ba0
Mangnex <127064046+Mangnex@users.noreply.github.com>
2026-06-02 21:14:19 +0200

    Update VIPLoader.lua

--- VIPLoader.lua
+++ VIPLoader.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 15696848933 or game.PlaceId == 123063406055708 then
   return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/HL",Method="GET"}).Body)()
-elseif game.PlaceId == 5233782396 or game.PlaceId == 136015760267602 then
+elseif game.PlaceId == 5233782396 or game.PlaceId == 136015760267602 or game.PlaceId == 125036322376647 then
   getgenv().SCRIPT_KEY = ""
  return loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/ecb44bf5d377aae4932921c46820a9d76446200a0a2aef2477c1cbf24a99473e/download"))()
 else
