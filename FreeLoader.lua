4990b6b922f554c3c4407fb4f477e2e168e8cd5b
Mangnex <127064046+Mangnex@users.noreply.github.com>
2026-07-22 19:26:01 +0200

    Update FreeLoader.lua

--- FreeLoader.lua
+++ FreeLoader.lua
@@ -2,5 +2,5 @@ if game.PlaceId == 3431407618 then
     return loadstring(request({Url="https://lunar-rest-api.vercel.app/script/Isle10",Method="GET"}).Body)()
 else
    getgenv().SCRIPT_KEY = ""
-   return loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/441457bbbb948e52667ad5bbc45b77324023cf493ef13aa448b758022e06d480/download"))()
+   return loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/6c2d6a099fbfccd234496ca4916f3aef40228e4caa4d64bb80c9fe6e27577967/download"))()
 end
